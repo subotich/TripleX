1a645e7ced63a31e224e0ac800c8ae1f3fb62320
subotich <suboti.ch@icloud.com>
2019-05-03 05:48:48 +0200

    Add project files.

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,65 +1,20 @@
+// TripleX.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
 #include <iostream>
-
-void PrintIntroduction(int Difficulty)
-{
-    std::cout << "\n\nYou are a secret agent breaking into a level " << Difficulty;
-    std::cout << " secure server room...\nEnter the correct code to continue...\n\n";
-}
-
-bool PlayGame(int Difficulty)
-{
-    PrintIntroduction(Difficulty);
-
-    const int CodeA = rand();
-    const int CodeB = rand();
-    const int CodeC = rand();
-    
-    const int CodeSum = CodeA + CodeB + CodeC;
-    const int CodeProduct = CodeA * CodeB * CodeC;
-
-
-    // Print CodeSum and CodeProduct to the terminal
-    std::cout << "+ There are 3 numbers in the code";
-    std::cout << "\n+ The codes add-up to: " << CodeSum;
-    std::cout << "\n+ The codes multiply to give: " << CodeProduct << std::endl;
-
-    // Store player guess
-    int GuessA, GuessB, GuessC;
-    std::cin >> GuessA >> GuessB >> GuessC;
-
-    int GuessSum = GuessA + GuessB + GuessC;
-    int GuessProduct = GuessA * GuessB * GuessC;
-
-    // Check if the players guess is correct
-    if (GuessSum == CodeSum && GuessProduct == CodeProduct)
-    {
-        std::cout << "\n*** Well done agent! You have extracted a file! Keep going! ***";
-        return true;
-    }
-    else
-    {
-        std::cout << "\n*** You entered the wrong code! Careful agent! Try again! ***";
-        return false;
-    }
-}
 
 int main()
 {
-    int LevelDifficulty = 1;
-    int const MaxDifficulty = 5;
-
-    while (LevelDifficulty <= MaxDifficulty) // Loop game until all levels completed
-    {
-        bool bLevelComplete = PlayGame(LevelDifficulty);
-        std::cin.clear(); // Clears any errors
-        std::cin.ignore(); // Discards the buffer
-
-        if (bLevelComplete) 
-        {
-            ++LevelDifficulty;
-        }
-        
-    }
-    std::cout << "\n*** Great work agent! You got all the files! Now get out of there! ***\n";
-    return 0;
+    std::cout << "Hello World!\n"; 
 }
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
